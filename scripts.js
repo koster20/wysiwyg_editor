ac3765e7e920164f813f1de6fb8d637edc168d46
koster20 <koster20011@gmail.com>
2021-09-01 12:48:22 +0400

    created base code

--- scripts.js
+++ scripts.js
@@ -1,67 +1,165 @@
+// define vars
 const editor = document.getElementsByClassName('editor')[0];
 const toolbar = editor.getElementsByClassName('toolbar')[0];
-const buttons = toolbar.querySelectorAll('btn:not(.has-submenu)')[0];
+const buttons = toolbar.querySelectorAll('.btn:not(.has-submenu)');
 const contentArea = editor.getElementsByClassName('content-area')[0];
 const visuellView = contentArea.getElementsByClassName('visuell-view')[0];
 const htmlView = contentArea.getElementsByClassName('html-view')[0];
-const modal =document.getElementsByClassName('modal')[0];
+const modal = document.getElementsByClassName('modal')[0];
 
+// add active tag event
+document.addEventListener('selectionchange', selectionChange);
 
-for(let i=0;i<buttons.length;i++){
-    let button =buttons[i];
-    button.addEventListener('click',function(e){
-        let action =this.dataset.action;
-        switch(action){
-            case 'code':
-                execCodeAction(this, editor);
-                break;
-            case 'createLink':
-                execLinkAction();
-                break;
-            default:
-                execDefaultAction(action);
-        }
-
-    });
-}
-function execDefaultAction(action) {
-    document.execCommand(action, false);
-}
-function execCodeAction(button,editor){
-    if(button.classList.contains('active')){
-        visuellView.innerHTML = htmlView.value;
-        htmlView.getElementsByClassName.display = 'none';
-        visuellView.style.display = 'block';
+// add toolbar button actions
+for(let i = 0; i < buttons.length; i++) {
+  let button = buttons[i];
+  
+  button.addEventListener('click', function(e) {
+    let action = this.dataset.action;
     
-        button.classList.remove('active');
+    switch(action) {
+      case 'code':
+        execCodeAction(this, editor);
+        break;
+      case 'createLink':
+        execLinkAction();
+        break;
+      default:
+        execDefaultAction(action);
     }
-    else{
-        htmlView.innerHTML = visuellView.innerHTML;
-        visuellView.style.display = 'none';
-        htmlView.style.display = 'block';
-
-        button.classList.addEventListener('active');
-    }
+    
+  });
 }
 
-function execLinkAction() {
-    modal.style.display = 'block';
-    let selection =saveSelection();
+// this function toggles between visual and html view
+function execCodeAction(button, editor) {
 
+  if(button.classList.contains('active')) { // show visuell view
+    visuellView.innerHTML = htmlView.value;
+    htmlView.style.display = 'none';
+    visuellView.style.display = 'block';
 
-    let submit = modal.querySelectorAll('button.done')[0];
-    let close = modal.querySelectorAll('.close')[0];
+    button.classList.remove('active');     
+  } else {  // show html view
+    htmlView.innerText = visuellView.innerHTML;
+    visuellView.style.display = 'none';
+    htmlView.style.display = 'block';
+
+    button.classList.add('active'); 
+  }
 }
 
+// add link action
+function execLinkAction() {  
+  modal.style.display = 'block';
+  let selection = saveSelection();
 
-function saveSelection(){
+  let submit = modal.querySelectorAll('button.done')[0];
+  let close = modal.querySelectorAll('.close')[0];
+  
+  // done button active => add link
+  submit.addEventListener('click', function() {
+    let newTabCheckbox = modal.querySelectorAll('#new-tab')[0];
+    let linkInput = modal.querySelectorAll('#linkValue')[0];
+    let linkValue = linkInput.value;
+    let newTab = newTabCheckbox.checked;    
+    
+    restoreSelection(selection);
+    
+    if(window.getSelection().toString()) {
+      let a = document.createElement('a');
+      a.href = linkValue;
+      if(newTab) a.target = '_blank';
+      window.getSelection().getRangeAt(0).surroundContents(a);
+    }
+
+    modal.style.display = 'none';
+    linkInput.value = '';
+    
+    // deregister modal events
+    submit.removeEventListener('click', arguments.callee);
+    close.removeEventListener('click', arguments.callee);
+  });  
+  
+  // close modal on X click
+  close.addEventListener('click', function() {
+    let linkInput = modal.querySelectorAll('#linkValue')[0];
+    
+    modal.style.display = 'none';
+    linkInput.value = '';
+    
+    // deregister modal events
+    submit.removeEventListener('click', arguments.callee);
+    close.removeEventListener('click', arguments.callee);
+  });
+}
+
+// executes normal actions
+function execDefaultAction(action) {
+  document.execCommand(action, false);
+}
+
+// saves the current selection
+function saveSelection() {
     if(window.getSelection) {
         sel = window.getSelection();
         if(sel.getRangeAt && sel.rangeCount) {
             let ranges = [];
-            for(var i=0,len =sel.rangeCount; i<len; ++i){
-                ranges.push
+            for(var i = 0, len = sel.rangeCount; i < len; ++i) {
+                ranges.push(sel.getRangeAt(i));
             }
+            return ranges;
+        }
+    } else if (document.selection && document.selection.createRange) {
+        return document.selection.createRange();
+    }
+    return null;
+}
+
+// loads a saved selection
+function restoreSelection(savedSel) {
+    if(savedSel) {
+        if(window.getSelection) {
+            sel = window.getSelection();
+            sel.removeAllRanges();
+            for(var i = 0, len = savedSel.length; i < len; ++i) {
+                sel.addRange(savedSel[i]);
+            }
+        } else if(document.selection && savedSel.select) {
+            savedSel.select();
         }
     }
+}
+
+// sets the current format buttons active/inactive
+function selectionChange() {
+  
+  for(let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.classList.remove('active');
+  }
+  
+  parentTagActive(window.getSelection().anchorNode.parentNode);
+}
+
+function parentTagActive(elem) {
+  if(elem.classList.contains('visuell-view')) return false;
+  
+  let toolbarButton;
+  
+  // active by tag names
+  let tagName = elem.tagName.toLowerCase();
+  toolbarButton = document.querySelectorAll(`.toolbar .btn[data-tag-name="${tagName}"]`)[0];
+  if(toolbarButton) {
+    toolbarButton.classList.add('active');
+  }
+  
+  // active by text-align
+  let textAlign = elem.style.textAlign;
+  toolbarButton = document.querySelectorAll(`.toolbar .btn[data-style="textAlign:${textAlign}"]`)[0];
+  if(toolbarButton) {
+    toolbarButton.classList.add('active');
+  }
+  
+  return parentTagActive(elem.parentNode);
 }
